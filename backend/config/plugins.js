7c77bcba8aa937c770dc4a4d8b3ffec5eb3147d1
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2022-12-31 18:02:58 +0100

    Strapi fixed

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,10 +2,10 @@ module.exports = ({ env }) => ({
 	'vercel-deploy': {
 		enabled: true,
 		config: {
-			deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-			apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-			appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-			teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
+			deployHook: env('VERCEL_DEPLOY_PLUGIN_HOOK'),
+			apiToken: env('VERCEL_DEPLOY_PLUGIN_API_TOKEN'),
+			appFilter: env('VERCEL_DEPLOY_PLUGIN_APP_FILTER'),
+			teamFilter: env('VERCEL_DEPLOY_PLUGIN_TEAM_FILTER'),
 			roles: ['strapi-super-admin'],
 		},
 	},
