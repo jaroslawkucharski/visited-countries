075c0443af8ac9b528fed55f73c13ff9f694a2a6
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-01-06 00:26:17 +0100

    code fixed

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,12 +1,7 @@
-import { authToken } from 'config/authToken'
 import { client } from 'config/client'
 
 export const loginRequest = (identifier: string, password: string) =>
-	client.post(
-		`/api/auth/local`,
-		{
-			identifier,
-			password,
-		},
-		authToken,
-	)
+	client.post(`/api/auth/local`, {
+		identifier,
+		password,
+	})
