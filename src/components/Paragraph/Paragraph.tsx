022cc5644fcab885862f2090d167969f9eac9d8b
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-01-06 17:16:28 +0100

    Paragraph component created

---
+++ src/components/Paragraph/Paragraph.tsx
@@ -0,0 +1,25 @@
+import { FC, ReactNode } from 'react'
+
+import { HeadingComponent } from './Paragraph.styles'
+
+interface ParagraphProps {
+	type?: 'default' | 'label' | 'error'
+	children: ReactNode
+	size?: 'small' | 'medium' | 'big'
+	align?: 'left' | 'center' | 'right'
+}
+
+export const Paragraph: FC<ParagraphProps> = ({
+	type = 'default',
+	children,
+	size = 'medium',
+	align = 'center',
+}) => (
+	<HeadingComponent
+		type={type}
+		size={size}
+		align={align}
+	>
+		{children}
+	</HeadingComponent>
+)
