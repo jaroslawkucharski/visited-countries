10af40856f2c29e4e80fcaaf513e970bd8b7b234
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-01-06 21:15:17 +0100

    react-simple-map package added

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,1 +1,21 @@
-export const Dashboard = () => <p>DASHBOARD</p>
+import topology from 'assets/topology.json'
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
+
+export const Dashboard = () => (
+	<div>
+		<ComposableMap>
+			<Geographies geography={topology}>
+				{({ geographies }) =>
+					geographies.map(geo => (
+						<Geography
+							key={geo.rsmKey}
+							geography={geo}
+							fill="#FF5533"
+							stroke="#000000"
+						/>
+					))
+				}
+			</Geographies>
+		</ComposableMap>
+	</div>
+)
