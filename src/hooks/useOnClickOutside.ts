9f666df8a773c4ed38e4b145f80e6d0832747492
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-01-07 12:13:36 +0100

    created useOnClickOutside hook

---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,26 @@
+import { RefObject, useEffect } from 'react'
+
+type Event = MouseEvent | TouchEvent
+
+export const useOnClickOutside = (
+	ref: RefObject<HTMLElement>,
+	handler: (event: Event) => void,
+): void => {
+	useEffect(() => {
+		const listener = (event: Event) => {
+			if (!ref.current || ref.current.contains(event.target as Node)) {
+				return
+			}
+
+			handler(event)
+		}
+
+		document.addEventListener('mousedown', listener)
+		document.addEventListener('touchstart', listener)
+
+		return () => {
+			document.removeEventListener('mousedown', listener)
+			document.removeEventListener('touchstart', listener)
+		}
+	}, [ref, handler])
+}
