afd5bad994c31cd7af37e43df6867565e55e72f0
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-01-09 15:57:09 +0100

    MobileMenu created

---
+++ src/layouts/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,11 @@
+import { HiGlobe, HiUserCircle, HiViewList } from 'react-icons/hi'
+
+import { MobileMenuComponent } from './MobileMenu.style'
+
+export const MobileMenu = () => (
+	<MobileMenuComponent>
+		<HiGlobe />
+		<HiViewList />
+		<HiUserCircle />
+	</MobileMenuComponent>
+)
