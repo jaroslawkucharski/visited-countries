4f327e353537ae2b00197765a1755f67e2e1ed79
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-01-13 01:07:54 +0100

    code fixed

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -4,14 +4,16 @@ import { Heading } from './Heading'
 
 describe('components/Header', () => {
 	it('should be H1', () => {
-		const { getByTestId } = render(<Heading>Hello world</Heading>)
+		const { getByTestId, container } = render(<Heading>Hello world</Heading>)
 
 		expect(getByTestId('heading')).toBeInTheDocument()
+		expect(container.querySelector('h1')).toBeInTheDocument()
 	})
 
 	it('should be H2', () => {
-		const { getByTestId } = render(<Heading level={2}>Hello world</Heading>)
+		const { getByTestId, container } = render(<Heading level={2}>Hello world</Heading>)
 
 		expect(getByTestId('heading')).toBeInTheDocument()
+		expect(container.querySelector('h2')).toBeInTheDocument()
 	})
 })
