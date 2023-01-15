210324802b183fc10d3bd94d876d135caa03133c
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-01-15 10:56:45 +0100

    heading added to Profile & List

--- src/pages/DashboardList/DashboardList.tsx
+++ src/pages/DashboardList/DashboardList.tsx
@@ -1,10 +1,17 @@
-import { Input, Spacer } from 'components'
+import { Heading, Input, Spacer } from 'components'
+import { useTranslation } from 'react-i18next'
 
 import { ProfileColumnComponent } from './DashboardList.styles'
 
 export const DashboardList = () => {
+	const { t } = useTranslation()
+
 	return (
 		<ProfileColumnComponent>
+			<Heading>{t('word.list')}</Heading>
+
+			<Spacer type="vertical" />
+
 			<Input
 				label="Search"
 				name="searcher"
