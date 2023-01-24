3f7a69bc6de4cf7aa3d77ed323744a8901151a9d
Edukacja <edukacja@Landingi-MacBook-Pro.local>
2023-01-25 00:14:16 +0100

    code fixed

--- src/hooks/useService.ts
+++ src/hooks/useService.ts
@@ -50,8 +50,8 @@ export const useService = ({
 				}
 
 				successCallback?.()
-			} catch ({ code }) {
-				toastNotify(errorToast || i18next.t(ERRORS[code] || 'toasts.error'), 'error')
+			} catch ({ code }: any) {
+				toastNotify(errorToast || i18next.t(ERRORS[code as string] || 'toasts.error'), 'error')
 
 				errorCallback?.()
 			}
